df5ffd747155999f29ceb95e5795dffa454d6f5f
Caroline Santos <112205831+carolinesanttos@users.noreply.github.com>
2024-08-28 21:06:34 -0300

    Iniciando facade
    Falta apenas logout do usuário

--- vendaingressos/src/br/uefs/ecomp/vendaingressos/facade/Facade.java
+++ vendaingressos/src/br/uefs/ecomp/vendaingressos/facade/Facade.java
@@ -8,45 +8,45 @@ import br.uefs.ecomp.vendaingressos.model.Usuario;
 
 public class Facade {
 
-//    private Controller controller;
-//
-//    public Facade() {
-//        this.controller = new Controller();
-//    }
-//
-//    // Chamadas do Controller relacionadas ao usuário
-//    public Usuario cadastrarUsuario(String login, String senha, String nome, String cpf, String email) {
-//        return controller.cadastrarUsuario(login, senha, nome, cpf, email);
-//    }
-//
-//    public Usuario loginUsuario(String login, String senha) {
-//        return controller.loginUsuario(login, senha);
-//    }
-//
+    private Controller controller;
+
+    public Facade() {
+        this.controller = new Controller();
+    }
+
+    // Chamadas do Controller relacionadas ao usuário
+    public Usuario cadastrarUsuario(String login, String senha, String nome, String cpf, String email) {
+        return controller.cadastrarUsuario(login, senha, nome, cpf, email);
+    }
+
+    public Usuario loginUsuario(String login, String senha) {
+        return controller.loginUsuario(login, senha);
+    }
+
 //    public void logoutUsuario() {
 //        controller.logoutUsuario();
 //    }
-//
-//    // Chamadas do Controller relacionadas ao evento
-//    public Evento cadastrarEvento(String nome, String descricao, String data) {
-//        return controller.cadastrarEvento(nome, descricao, data);
-//    }
-//
-//    public List<Evento> listarEventosDisponiveis() {
-//        return controller.listarEventosDisponiveis();
-//    }
-//
-//    // Chamadas do Controller relacionadas ao ingresso
-//    public Ingresso comprarIngresso(String nomeEvento) {
-//        return controller.comprarIngresso(nomeEvento);
-//    }
-//
-//    public List<Ingresso> listarIngressosComprados() {
-//        return controller.listarIngressosComprados();
-//    }
-//
-//    public void cancelarCompraIngresso(String idIngresso) {
-//        controller.cancelarCompraIngresso(idIngresso);
-//    }
+
+    // Chamadas do Controller relacionadas ao evento
+    public Evento cadastrarEvento(String nome, String descricao, String data) {
+        return controller.cadastrarEvento(nome, descricao, data);
+    }
+
+    public List<Evento> listarEventosDisponiveis() {
+        return controller.listarEventosDisponiveis();
+    }
+
+    // Chamadas do Controller relacionadas ao ingresso
+    public Ingresso comprarIngresso(String nomeEvento) {
+        return controller.comprarIngresso(nomeEvento);
+    }
+
+    public List<Ingresso> listarIngressosComprados() {
+        return controller.listarIngressosComprados();
+    }
+
+    public void cancelarCompraIngresso(String idIngresso) {
+        controller.cancelarCompraIngresso(idIngresso);
+    }
 }
 
